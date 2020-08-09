5d57d2ff559631a8ff6811d59d0edb08d112567a
candescent-sequence-1308 <amandaashmen@gmail.com>
2020-08-09 21:52:45 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,15 @@
 //define functions here
 
 function getIt() {
-  
+  $('p').on("click", function(){
+   alert("Hey!");
+});
+}
+
+function frameIt() {
+  document.on("load", function(){
+   
+});
 }
 
 
